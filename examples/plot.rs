45a44c09276d9ebca7e379e6c3cdf9f135471f0d
Malik Olivier Boussejra <malik@boussejra.com>
2018-03-09 18:39:24 +0900

    plot.rs: Print LineCollection

--- examples/plot.rs
+++ examples/plot.rs
@@ -3,14 +3,20 @@ extern crate matplotrs;
 use matplotrs::app::App;
 use matplotrs::figure::Figure;
 use matplotrs::axes::AxesBuilder;
-use matplotrs::color::{BLACK};
+use matplotrs::color::RED;
 use matplotrs::line::Line;
+use matplotrs::line_collection::LineCollectionBuilder;
+
 
 fn main() {
     let mut app = App::new();
     let mut fig = Figure::new();
-    let mut axes = AxesBuilder::new().with_edgecolor(BLACK).build();
+    let mut axes = AxesBuilder::new().with_edgecolor(RED).build();
     axes.add_child(Line::new((0.0, 0.0), (0.5, 0.5)));
+    let linecol = LineCollectionBuilder::new().with_vertices(
+        vec![(-1.0, -0.7), (-0.7, -0.6), (-0.4, -0.75), (-0.1, -0.8)]
+    ).build();
+    axes.add_child(linecol);
     fig.add_child(axes);
     app.add_figure(fig);
     app.render().unwrap();
