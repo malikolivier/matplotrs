90de7dc3043a939e3828f85d0aa5b002edbf988a
Malik Olivier Boussejra <malik@boussejra.com>
2018-03-10 15:53:50 +0900

    Add draw_text method to Backend trait

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -4,6 +4,7 @@ pub trait Backend {
     fn new() -> Self;
     fn new_figure(&mut self, title: &str, size: &(f64, f64));
     fn draw_path(&mut self, path: &Path) -> Result<(), Self::Err>;
+    fn draw_text(&mut self, text: &Text) -> Result<(), Self::Err>;
     fn show(self) -> Result<i32, Self::Err>;
 }
 
@@ -12,4 +13,10 @@ pub struct Path {
     pub closed: bool,
     pub line_color: Option<(f64, f64, f64, f64)>,
     pub fill_color: Option<(f64, f64, f64, f64)>,
+}
+
+pub struct Text<'s> {
+    pub point: (f64, f64),
+    pub text: &'s str,
+    pub font_size: f32,
 }
--- src/backend/dummy.rs
+++ src/backend/dummy.rs
@@ -12,9 +12,13 @@ impl matplotrs_backend::Backend for DummyBackend {
         DummyBackend { fig: () }
     }
 
-    fn new_figure(&mut self, title: &str, size: &(f64, f64)) {}
+    fn new_figure(&mut self, _title: &str, _size: &(f64, f64)) {}
 
-    fn draw_path(&mut self, path: &matplotrs_backend::Path) -> Result<(), Self::Err> {
+    fn draw_path(&mut self, _path: &matplotrs_backend::Path) -> Result<(), Self::Err> {
+        Ok(())
+    }
+
+    fn draw_text(&mut self, _text: &matplotrs_backend::Text) -> Result<(), Self::Err> {
         Ok(())
     }
 
